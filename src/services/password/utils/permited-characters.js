b2c61cd1ecc7c808da925f2c3bd44d00ce19b51a
RafaelValiati <131008596+RafaelValiati@users.noreply.github.com>
2024-05-18 00:56:55 -0300

    first commit

---
+++ src/services/password/utils/permited-characters.js
@@ -0,0 +1,22 @@
+async function permittedCharacters(){
+    let permitted = []
+
+    if(process.env.UPPERCASE_LETTERS === "true")
+        permitted.push(..."ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+    
+
+    if(process.env.LOWERCASE_LETTERS === "true")
+        permitted.push(..."abcdefghijklmnopqrstuvwxyz")
+    
+
+    if(process.env.NUMBERS === "true")
+        permitted.push(..."1234567890")
+    
+
+    if(process.env.SPECIAL_CHARACTERS === "true")
+        permitted.push(..."!@#$%¨&*()")
+    
+    return permitted
+}
+
+export default permittedCharacters
